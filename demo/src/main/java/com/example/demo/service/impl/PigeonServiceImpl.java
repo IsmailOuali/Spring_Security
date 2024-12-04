d112385b7c059882d53061e9d40f97acd65e5679
unknown <ismail.ouali@ump.ac.ma>
2024-12-04 09:33:30 +0100

    Adding interfaces for pigeon servie

--- demo/src/main/java/com/example/demo/service/impl/PigeonServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/impl/PigeonServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.Pigeon;
 import com.example.demo.repository.PigeonRepository;
+import com.example.demo.service.PigeonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class PigeonService {
+public class PigeonServiceImpl implements PigeonService {
 
     @Autowired
     private PigeonRepository pigeonRepository;
